09f69a5058fc278a0e5d2cd7d0a17ca97b8697e9
sfaa <ym6772003@gmail.com>
2024-11-22 18:10:03 -0800

    f

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import MobileNavbar from "./MobileNavbar";
-import { useRouter } from "next/router";
+
 
 function Header() {
 
