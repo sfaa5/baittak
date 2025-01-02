e0aa78117e96fb30e77135d52e0f981d772d2a0e
sfaa <ym6772003@gmail.com>
2025-01-01 19:04:14 -0800

    fix: correct comment formatting in Agency page component

--- app/[locale]/(root)/Agency/page.tsx
+++ app/[locale]/(root)/Agency/page.tsx
@@ -36,7 +36,7 @@ async function Page( { searchParams }: PageProps) {
   const page = queryParams.get('page') ?? '1';
   const per_page = queryParams.get('per_page') ?? '5';
   const start = (Number(page) - 1) * Number(per_page); // 0, 5, 10 ..
-  const end = start + Number(per_page); // 5, 10, 15 ...
+  const end = start + Number(per_page); // 5, 10, 15 ..
 
   const city = queryParams.get('city') ?? '';
   const service = queryParams.get('service') ?? '';
