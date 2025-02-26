370a9f9e4d00e402e24c558d78a0d2914c8544ea
sfaa <ym6772003@gmail.com>
2025-02-25 17:06:26 -0800

    solve type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,7 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/nextAuth";
-import NextAuth from "next-auth"
 
+const handler = NextAuth(authOptions as NextAuthOptions);
 
-const handler = NextAuth(authOptions);
-
-
-
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
