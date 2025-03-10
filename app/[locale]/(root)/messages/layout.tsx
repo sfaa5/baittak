cd2fd7852888cbe3b291bb4e226795a10db31478
sfaa <ym6772003@gmail.com>
2025-03-10 10:57:11 -0700

    fix message

--- app/[locale]/(root)/messages/layout.tsx
+++ app/[locale]/(root)/messages/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({
     <div className="container 2xl:px-[120px] flex flex-col pt-5 gap-5  ">
       <Buttons />
 
-      <div className="flex flex-row h-[74vh] shadow-lg border-primary border-[1px] rounded-lg bg-gray-400  backdrop-filter backdrop-blur-lg bg-opacity-0 overflow-hidden">
+      <div className="relative flex flex-row h-[74vh] shadow-lg border-primary border-[1px] rounded-lg bg-gray-400  backdrop-filter backdrop-blur-lg bg-opacity-0 overflow-hidden">
         <Sidebar />
         {children}
       </div>
